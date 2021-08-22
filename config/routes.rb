e9e5238d544f22e3a7fb195f0041ae91cbec72ca
Hi-yano <begin_2020@icloud.com>
2021-08-22 21:41:22 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
+# Rails.application.routes.draw do
+#   get 'spa/index'
+#   resources :users
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
+# 以下に修正
 Rails.application.routes.draw do
-  get 'spa/index'
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'spa#index'
+  resources :users,only: [:index, :create, :update, :destroy]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
